a7fabf2423213166af4227591fd6cadb8399ed7a
Abdulmujib <asiyanbiakorede@gmail.com>
2021-08-20 11:57:52 +0100

    part4 exercise 4.1-4.2

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogRouter = require('./controller/blogs')
+const logger = require('./utils/logger')
+
+
+
+const {mongoUrl} = config
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => logger.info('connected to server'))
+    .catch(err => logger.error('error connecting to momgodb:',err.message))
+
+app.use(cors())
+app.use(express.json())
+
+
+app.use('/api/blog',blogRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,42 +1,8 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl =  'mongodb+srv://firstdb:slimani007@cluster0.tsal5.mongodb.net/Blog_database?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+ const app = require('./app')
+ const logger = require('./utils/logger')
+ const config = require('./utils/config')
+const http = require('http')
+const server = http.createServer(app)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 }) 
